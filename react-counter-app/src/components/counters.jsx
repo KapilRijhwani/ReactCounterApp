405b9d13beae664b85538159a67075bd70cb43a5
KapilRijhwani <kapil.rijhwani@gmail.com>
2019-12-24 03:08:31 +0530

    Included Counter and Counters component

---
+++ react-counter-app/src/components/counters.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 1 },
+      { id: 3, value: 2 },
+      { id: 4, value: 0 }
+    ]
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </div>
+    );
+  }
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(
+      counter => counter.id !== counterId
+    );
+    this.setState({ counters });
+  };
+}
+
+export default Counters;
