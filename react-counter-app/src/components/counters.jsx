988c2880f3b1b45633262983de7d644b01831224
KapilRijhwani <kapil.rijhwani@gmail.com>
2019-12-24 03:22:55 +0530

    Implemented Reset functionality

--- react-counter-app/src/components/counters.jsx
+++ react-counter-app/src/components/counters.jsx
@@ -10,23 +10,48 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
   render() {
     return (
       <div>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={this.handleReset}
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
             counter={counter}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
           />
         ))}
       </div>
     );
   }
+
+  handleReset = () => {
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = counterId => {
     const counters = this.state.counters.filter(
       counter => counter.id !== counterId
     );
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
     this.setState({ counters });
   };
 }
