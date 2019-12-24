6872884ef78497aab8878073cb74ce9259822cc7
KapilRijhwani <kapil.rijhwani@gmail.com>
2019-12-24 11:32:30 +0530

    Moved the state upto the root parent component

--- react-counter-app/src/App.js
+++ react-counter-app/src/App.js
@@ -1,8 +1,61 @@
 import React from "react";
 import "./App.css";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
-function App() {
-  return <div>Hello</div>;
+class App extends React.Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 1 },
+      { id: 3, value: 2 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleReset = () => {
+    if (this.state.counters.length === 0) return;
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(
+      counter => counter.id !== counterId
+    );
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar
+          totalCounters={
+            this.state.counters.filter(counter => counter.value > 0).length
+          }
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
