29c957c86c09a358050df77c82846b262c0ff630
KapilRijhwani <kapil.rijhwani@gmail.com>
2019-12-24 11:43:07 +0530

    Modified components to stateless functional components

--- react-counter-app/src/index.js
+++ react-counter-app/src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Counter from "./components/counter";
-import Counters from "./components/counters";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
